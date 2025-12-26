53cab4c1ba4ec5b75c2bed7936c8420df7dced7b
Lucas <lucasdsf7@gmail.com>
2025-12-26 08:53:57 -0300

    fixing warnings bug in ExatiSession

--- exati_routers.py
+++ exati_routers.py
@@ -46,8 +46,9 @@ class ExatiSession(requests.sessions.Session):
                 print(f'KeyError de RAIZ, {response}')
             response = self.ex_post(payload=payload, depth=depth + 1, warnings=warnings)
             message = response['RAIZ']['MESSAGES']['ERRORS']
-        if message and warnings:
-            print(f'{message}, depth = {depth}')
+        if message:
+            if warnings:
+                print(f'{message}, depth = {depth}')
             if depth > 3:
                 return response
             sleep(0.25)
@@ -61,6 +62,16 @@ class ConsultarAtributos():
     '''
     def __init__(self, session: ExatiSession):
         self.session = session
+        self.records: list[dict] = None
+
+    @property
+    def records(self):
+        '''
+        Assuring that property records is not None.
+        '''
+        if self.records is None:
+            self.export()
+        return self.records
 
     def export(self) -> list[dict]:
         '''
@@ -71,35 +82,39 @@ class ConsultarAtributos():
             'parser': 'json'
         }
         response = self.session.ex_post(payload=payload)
-        return response['RAIZ']['ATRIBUTOS']['ATRIBUTO']
+        self.records = response['RAIZ']['ATRIBUTOS']['ATRIBUTO']
 
     def name_to_records(self, name='NOME') -> dict[str, list[dict]]:
         '''
         Create a dict with key = name of attribute and values = records
         '''
-        records = self.export()
-        return {atb[name]: atb for atb in records}
+        self.export()
+        return {atb[name]: atb for atb in self.records}
 
 
 class IDsParqueServico():
     '''
     Router IDs Parque Servico.
     '''
-    def __init__(self, session: ExatiSession, atributos: ConsultarAtributos):
+    def __init__(self, session: ExatiSession):
         self.session = session
-        self.atributos = atributos
+        self.records = None
 
-    def export(self, names_attributes: list[str], filtros: str) -> list[dict]:
+    @property
+    def records(self):
+        '''
+        Assuring that property records is not None.
+        '''
+        if self.records is None:
+            self.export()
+        return self.records
+
+    def export(self, atb_ids: list[str] = None, filtros: str = '') -> list[dict]:
         '''
         Export records from API.
         '''
-        attributes_records = self.atributos.name_to_records()
-        atb_ids: list[str] = []
-        for name in names_attributes:
-            try:
-                atb_ids.append(str(attributes_records[name]['ID_ATRIBUTO']))
-            except KeyError:
-                print(f'Attribute {name} not in the records')
+        if atb_ids is None:
+            atb_ids: list[str] = []
         payload = {
             'CMD_IDS_PARQUE_SERVICO': 1,
             'CMD_COMMAND': 'ConsultarPontosServicos',
@@ -109,12 +124,12 @@ class IDsParqueServico():
             'parser': 'json'
         }
         response = self.session.ex_post(payload=payload)
-        return response['RAIZ']['PONTOS_SERVICOS']['PONTO_SERVICO']
+        self.records = response['RAIZ']['PONTOS_SERVICOS']['PONTO_SERVICO']
 
-    def name_to_records(self, names_attributes: list[str], filtros: str, name='ID_PONTO_SERVICO')\
+    def name_to_records(self, atb_ids: list[str] = None, filtros: str='', name='ID_PONTO_SERVICO')\
         -> dict[str, list[dict]]:
         '''
         Create a dict with key = name of attribute and values = records
         '''
-        records = self.export(names_attributes, filtros)
-        return {atb[name]: atb for atb in records}
+        self.export(atb_ids, filtros)
+        return {atb[name]: atb for atb in self.records}
